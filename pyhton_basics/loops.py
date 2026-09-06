2869852e4ae0d203c8ce0648976be6a0c3e1604c
K. Sathya Srri <karumudisathyasrri@gmail.com>
2026-09-06 11:09:03 +0530

    Add loops example in Python

---
+++ pyhton_basics/loops.py
@@ -0,0 +1,21 @@
+# Loops in Python
+
+# For loop
+print("For loop:")
+for i in range(1, 6):
+    print(i)
+
+# While loop
+print("\nWhile loop:")
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+# Sum of numbers from 1 to 10
+total = 0
+
+for i in range(1, 11):
+    total += i
+
+print("\nSum from 1 to 10:", total)
