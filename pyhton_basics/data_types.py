5719af4e50e6e65eca33bb946d6ba114da0eed07
K. Sathya Srri <karumudisathyasrri@gmail.com>
2026-09-06 11:07:12 +0530

    Add data types demonstration in data_types.py

---
+++ pyhton_basics/data_types.py
@@ -0,0 +1,18 @@
+# Data Types in Python
+
+name = "Sathya"       # String
+age = 19              # Integer
+cgpa = 8.81           # Float
+is_student = True     # Boolean
+
+print("Name:", name)
+print("Type:", type(name))
+
+print("Age:", age)
+print("Type:", type(age))
+
+print("CGPA:", cgpa)
+print("Type:", type(cgpa))
+
+print("Student:", is_student)
+print("Type:", type(is_student))
